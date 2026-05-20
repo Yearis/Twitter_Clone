8445c402c45523b07986aff828ddd883cd568774
Yearis <khush8751@gmail.com>
2026-05-20 23:51:18 +0530

    fix: fixed the bug in firebase config file and set up the DB on firebase

--- backend/src/config/firebaseConfig.ts
+++ backend/src/config/firebaseConfig.ts
@@ -1,9 +1,9 @@
-import * as admin from 'firebase-admin'
+import admin from 'firebase-admin'
 
 import serviceAccount from '../../serviceAccountKey.json';
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as any)
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
 });
 
 export const db = admin.firestore();
