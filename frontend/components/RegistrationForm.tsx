5ca613b09995780781367fe7ca1671c3d9d89f7e
Yearis <khush8751@gmail.com>
2026-05-21 18:52:01 +0530

    "fix: bug causing crashing of register"

--- frontend/components/RegistrationForm.tsx
+++ frontend/components/RegistrationForm.tsx
@@ -52,7 +52,7 @@ export default function RegistrationForm() {
                 return;
             }
 
-            setMessage(data);
+            setMessage(data.message);
 
         } catch (error) {
 
